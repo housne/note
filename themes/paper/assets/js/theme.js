ac15176c678ade2e340747ce448df42cbea0a384
Harry <housne@gmail.com>
2020-08-10 17:38:23 +0800

    update config and layout

--- themes/paper/assets/js/theme.js
+++ themes/paper/assets/js/theme.js
@@ -1,8 +1,6 @@
-autoTheme()
-
-function autoTheme() {
+~function() {
   const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
   if (isDarkMode) {
     document.body.classList.add('dark')
   }
-}
+}()
