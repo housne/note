11fc8613e8b66c26705d78bed0a46bb464e2c3be
Harry <housne@gmail.com>
2020-08-11 16:57:17 +0800

    use es build

--- themes/paper/assets/js/theme-switcher.js
+++ themes/paper/assets/js/theme-switcher.js
@@ -1,21 +1,21 @@
-~function() {
-  const THEME_SESSION_KEY = 'THEME_SESSION_KEY'
-  const themeFromCache = localStorage.getItem(THEME_SESSION_KEY)
-  let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches || themeFromCache === 'dark'
-  const themeSwitcher = document.getElementById('theme-switcher')
-  function setThemeEmoji(isDark) {
-    const themeEmoji = isDark ? '🌝' : '🌚'
-    themeSwitcher.innerHTML = themeEmoji
-  }
-  function toggleDarkMode() {
-    isDarkMode = !isDarkMode
-    setThemeEmoji(isDarkMode)
-    if (isDarkMode) {
-      document.body.classList.add('dark')
-    } else {
-      document.body.classList.remove('dark')
-    }
-  }
-  themeSwitcher.addEventListener('click', toggleDarkMode)
+const THEME_SESSION_KEY = 'THEME_SESSION_KEY'
+const themeFromCache = localStorage.getItem(THEME_SESSION_KEY)
+let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches || themeFromCache === 'dark'
+const themeSwitcher = document.getElementById('theme-switcher')
+function setThemeEmoji(isDark) {
+  const themeEmoji = isDark ? '🌝' : '🌚'
+  themeSwitcher.innerHTML = themeEmoji
+}
+function toggleDarkMode() {
+  isDarkMode = !isDarkMode
   setThemeEmoji(isDarkMode)
-}()
+  if (isDarkMode) {
+    document.body.classList.add('dark')
+    localStorage.setItem(THEME_SESSION_KEY, 'dark')
+  } else {
+    document.body.classList.remove('dark')
+    localStorage.removeItem(THEME_SESSION_KEY)
+  }
+}
+themeSwitcher.addEventListener('click', toggleDarkMode)
+setThemeEmoji(isDarkMode)
--- themes/paper/assets/js/theme.js
+++ themes/paper/assets/js/theme.js
@@ -1,6 +1,6 @@
-~function() {
-  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
-  if (isDarkMode) {
-    document.body.classList.add('dark')
-  }
-}()
+const THEME_SESSION_KEY = 'THEME_SESSION_KEY'
+const themeFromCache = localStorage.getItem(THEME_SESSION_KEY)
+let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches || themeFromCache === 'dark'
+if (isDarkMode) {
+  document.body.classList.add('dark')
+}
